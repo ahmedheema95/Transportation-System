f6f19aa1758f0abfb96121c6362385aab60d6d9d
Ahmed <ahmed_ibrahim95@ymail.com>
2020-07-01 14:59:06 +0200

    PassengerDataTest

--- Transportation System/Core/Domain/Passenger.cs
+++ Transportation System/Core/Domain/Passenger.cs
@@ -8,5 +8,8 @@ namespace Transportation_System.Core.Domain
     public class Passenger
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Telephone { get; set; }
     }
 }
