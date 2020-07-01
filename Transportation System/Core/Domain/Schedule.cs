13a2d5f27ff4488747bd9491256768c52103f2fb
eslam el-shenawy <i.a.shenawy@gmail.com>
2020-07-01 14:46:47 +0200

    Add Schedule Data

--- Transportation System/Core/Domain/Schedule.cs
+++ Transportation System/Core/Domain/Schedule.cs
@@ -8,5 +8,11 @@ namespace Transportation_System.Core.Domain
     public class Schedule
     {
         public int Id { get; set; }
+        public string Destination { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public string Startpoint { get; set; }
+        public string EndPoint { get; set; }
+        public TimeSpan ExpectedTime { get; set; }
     }
 }
---
+++ Transportation System/Migrations/20200701124558_AddScheduleColumns.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transportation_System.Migrations
+{
+    public partial class AddScheduleColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArrivalTime",
+                table: "Schedules",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DepartureTime",
+                table: "Schedules",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Destination",
+                table: "Schedules",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EndPoint",
+                table: "Schedules",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "ExpectedTime",
+                table: "Schedules",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Startpoint",
+                table: "Schedules",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArrivalTime",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "DepartureTime",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "Destination",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "EndPoint",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "ExpectedTime",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "Startpoint",
+                table: "Schedules");
+        }
+    }
+}
